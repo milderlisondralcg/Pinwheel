0e99d0705330844f4d8c8232a3390836aa0d91c3
milderlisondra025 <milder.lisondra@staysense.com>
2024-04-30 13:03:56 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,12 +1,16 @@
 # Scrape data
 # URL given
-
+    
 import requests
 import datetime
 import traceback
 import sys
 import os
 import json
+from pinwheel import Pinwheel
+
+Pinwheel.__init__()
+
 
 now = datetime.datetime.now()
 
@@ -24,11 +28,14 @@ print("Process started: " + str(now))
 # Set headers
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'}
 
-for i in range(last_page):
+#for i in range(last_page):
+for i in range(100,last_page):
     try:
         page_num = i + 1
+        print(page_num)
         # Retrieve base url
         url = 'https://www.biolegend.com/en-us/search-results?GroupID=&PageNum=' + str(page_num) + '&PageSize=' + str(products_per_page)
+        print(url)
         page = requests.get(url, headers=headers)
         
         # Create Beautiful Soup object
@@ -51,7 +58,7 @@ for i in range(last_page):
             
             #create filename
             json_file = "json/page" + str(page_num) + ".txt"
-
+            
             if os.path.exists(json_file):
                 os.remove(json_file)
                 print(f"File removed: {json_file}")
@@ -63,10 +70,14 @@ for i in range(last_page):
                     href = tag.get('href')
                     with open(json_file,"a", encoding="utf-8") as text:
                         text.write(href+"\n")
+            log_message = json_file + ":" + " Total links captured " + str(total_links_captured)
+            job_type = "Scrape"
+            Pinwheel.log_entry(job_type, log_message)
             
         except:
             print(traceback.format_exc())
             print("Could not get the list of urls from productsHolder element:")
+            Pinwheel.log_entry("hello")
             print(url)
     except Exception:
         print(traceback.format_exc())
