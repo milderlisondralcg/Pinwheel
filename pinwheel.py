a96bd5b0103f923355980ad0389f2808d288b30e
milderlisondra025 <milder.lisondra@staysense.com>
2024-07-09 18:01:27 -0700

    Modified function
    Modified function, log_entry. Added formatting and timestamp.

--- pinwheel.py
+++ pinwheel.py
@@ -1,20 +1,24 @@
 # Class Pinwheel
 
+import datetime
+
 class Pinwheel:
 
     LOG_PATH = "logs/"
+    ts = datetime.datetime.now()
     
-    def __init__():
-        print("hello from init")
+    #def __init__():
+        
         
     def create_log_file(job_type):
         path = job_type + "-"  + ".log"
 
     # Create a log entry
     def log_entry(job_type, log_message):
-        
+        #ts = datetime.datetime.now()
         log_file = Pinwheel.LOG_PATH + job_type + ".txt"
-
         with open(log_file,"a", encoding="utf-8") as text:
-            text.write(log_message + "\n")
+            text.write("**************************************\n")
+            text.write(str(Pinwheel.ts) + "\n" + job_type + ": " + log_message + "\n")
+            text.write("**************************************")
         
